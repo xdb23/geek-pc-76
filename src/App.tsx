af0d251708943a82e2b8013885a6d1ab7a0e8a4b
一心向北 <1579738787@qq.com>
2022-01-13 09:54:59 +0800

    准备-配置路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,20 @@
-export default function App() {
-  return <div>根组件</div>
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Login from "./pages/Login";
+
+
+
+function App() {
+  return (
+    <Router>
+      <div className='App'>
+        <Switch>
+          <Route path='./login' component={Login}></Route>
+          <Route path='./home' component={Layout}></Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
+
+export default App
